a795d4ed327876d68687f0e780bf86829f900803
DaniyarJakupov <ddr199305@gmail.com>
2018-04-26 14:54:40 +0200

    Fetch products from graphql server

--- src/screens/Products.js
+++ src/screens/Products.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator, FlatList, RefreshControl } from 'react-native';
 import { iOSColors } from 'react-native-typography';
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class Products extends Component {
-  state = {};
+  state = {
+    isRefreshing: false
+  };
+
+  refreshRequest = async () => {
+    this.setState({ isRefreshing: true });
+    await this.props.data.refetch();
+    this.setState({ isRefreshing: false });
+  };
+
+  renderItem = ({ item }) => <Text {...item}> {item.name} </Text>;
+
   render() {
+    const {
+      data: { loading, products }
+    } = this.props;
+
+    if (loading) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator color="red" size="large" />
+        </View>
+      );
+    }
+
     return (
       <View style={styles.root}>
-        <Text>Products</Text>
+        <FlatList
+          data={products}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={item => item.id}
+          renderItem={this.renderItem}
+          refreshControl={
+            <RefreshControl refreshing={this.state.isRefreshing} onRefresh={this.refreshRequest} />
+          }
+        />
 
         <ActionButton buttonColor="rgba(231,76,60,1)">
           <ActionButton.Item
@@ -28,9 +61,19 @@ class Products extends Component {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    backgroundColor: iOSColors.customGray,
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: iOSColors.customGray
   }
 });
-export default Products;
+
+const productsQuery = gql`
+  {
+    products {
+      name
+      pictureUrl
+      id
+      price
+    }
+  }
+`;
+
+export default graphql(productsQuery)(Products);
